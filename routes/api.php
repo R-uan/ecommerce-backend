a57e8314421b15ffa0284bd3d4250120716a9694
Ruan <ruan.oliveiralopes@hotmail.com>
2024-02-06 02:10:00 -0300

    better routing

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ManufacturersController;
+use App\Http\Controllers\ProductsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,8 +14,22 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
-*/
+ */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('/products')->group(function () {
+    Route::controller(ProductsController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::get('/search', 'search');
+        Route::get('/{id}', 'show')->where('id', '[0-9]+');
+        Route::post('/', 'store');
+        Route::delete('/{id}', 'destroy')->where('id', '[0-9]+');
+    });
+});
+
+/* Route::get('/', [ProductsController::class, 'index']);
+Route::get('/{id}', [ProductsController::class, 'show']); */
+Route::get('/manufacturers', [ManufacturersController::class, 'index']);
