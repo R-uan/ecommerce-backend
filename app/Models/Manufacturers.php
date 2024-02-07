3f23d388db18f3a594e07dd2baeacfdf73cfecc0
Ruan <ruan.oliveiralopes@hotmail.com>
2024-02-07 01:05:21 -0300

    $hidden and $fillable

--- app/Models/Manufacturers.php
+++ app/Models/Manufacturers.php
@@ -8,7 +8,8 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Manufacturers extends Model {
     use HasFactory;
-    protected $hidden = ['created_at', 'updated_at'];
+    protected $hidden   = ['created_at', 'updated_at'];
+    protected $fillable = ['name', 'email', 'website'];
     public function products(): HasMany {
         return $this->hasMany(Products::class);
     }
