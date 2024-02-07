bd99bad1b0b807b073d2e736e6c67dff0b104fee
Ruan <ruan.oliveiralopes@hotmail.com>
2024-02-07 01:04:39 -0300

    Re-alocation of files

--- app/Services/Filters/ProductsQuery.php
+++ app/Services/Filters/ProductsQuery.php
@@ -1,5 +1,5 @@
 <?php
-namespace App\Services;
+namespace App\Services\Filters;
 
 class ProductsQuery extends QueryFilter {
     protected $table         = 'products';
--- app/Services/Filters/QueryFilter.php
+++ app/Services/Filters/QueryFilter.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Services;
+namespace App\Services\Filters;
 
 use Illuminate\Http\Request;
 
