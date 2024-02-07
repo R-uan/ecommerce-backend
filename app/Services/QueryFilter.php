3d44310b16107d59824f9ce5aec4a5eebfea1130
Ruan <ruan.oliveiralopes@hotmail.com>
2024-02-06 22:21:51 -0300

    query filter

---
+++ app/Services/QueryFilter.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Http\Request;
+
+class QueryFilter {
+    protected $table         = '';
+    protected $allowedParams = [];
+    protected $columnMap     = [];
+
+    protected $operatorMap = [
+        'lk'  => 'ilike',
+        'eq'  => '=',
+        'lt'  => '<',
+        'lte' => '<=',
+        'gt'  => '>',
+        'gte' => '>=',
+    ];
+
+    public function transform(Request $request) {
+        $fullQuery = [];
+        foreach ($this->allowedParams as $param => $operators) {
+            $query = $request->query($param);
+            if (!isset($query)) {
+                continue;
+            }
+            $column = $this->columnMap[$param] ?? $param;
+            foreach ($operators as $operator) {
+                if (isset($query[$operator])) {
+                    $foundOperator = $this->operatorMap[$operator];
+                    if ($foundOperator == 'ilike') {
+                        $value       = "%" . $query[$operator] . "%";
+                        $fullQuery[] = [$this->table . '.' . $column, $foundOperator, $value];
+                    } else {
+                        $fullQuery[] = [$this->table . '.' . $column, $this->operatorMap[$operator], $query[$operator]];
+                    }
+                }
+            }
+
+        }
+        return $fullQuery;
+    }
+}
