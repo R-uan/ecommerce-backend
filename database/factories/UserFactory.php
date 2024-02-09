87601ef8ff28a006ae387e3699bcab42790a78bc
Ruan <ruan.oliveiralopes@hotmail.com>
2024-02-09 04:12:07 -0300

    user migration change

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,7 +3,6 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
@@ -22,11 +21,12 @@ class UserFactory extends Factory {
      */
     public function definition(): array {
         return [
-            'name'              => fake()->name(),
-            'email'             => "email@email.com",
-            'email_verified_at' => now(),
+            'first_name'        => $this->faker->firstName(),
+            'last_name'         => $this->faker->lastName(),
+            'email'             => "admin@admin.com",
             'role'              => 'ADMIN',
-            'password'          => static::$password ??= Hash::make('password'),
+            'password'          => 'admin',
+            'email_verified_at' => now(),
             'remember_token'    => Str::random(10),
         ];
     }
