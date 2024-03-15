b679adfcce57ec4f6bd97108302ef3ca0fb62cb5
Ruan <ruan.oliveiralopes@hotmail.com>
2024-03-15 02:50:47 -0300

    more test cases
    - update request
    - delete request
    - search queries
    - minatures request
    - some miniature request

--- tests/Feature/RequestsProductsTest.php
+++ tests/Feature/RequestsProductsTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Feature;
 
-use App\Models\Products;
 use Database\Seeders\DatabaseSeeder;
 use Faker\Factory as FakerFactory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -11,26 +10,30 @@ use Tests\TestCase;
 
 class RequestsProductsTest extends TestCase {
   use RefreshDatabase;
+  protected $token;
 
+  /**
+   * Resets the id sequence
+   * Seeds the database
+   *
+   * Creates a token for the request that require authentication.
+   * - Uses the user from the seeding to authenticate.
+   * - Asserts the token is not null.
+   */
   public function setUp(): void {
     parent::setUp();
     \DB::statement('ALTER SEQUENCE products_id_seq RESTART WITH 1');
     $this->seed(DatabaseSeeder::class);
+    $this->token = auth()->attempt(['email' => 'admin@admin.com', 'password' => 'admin']);
+    $this->assertNotNull($this->token);
   }
 
   /**
-   * Creates a token for the authentication:
-   * - Expects the token to not be null.
-   *
-   * Sends a get request to /api/admin/products.
-   * - Expects a CREATED (201) response.
+   * GET request to /api/products.
+   * - Asserts that the response status code is OK (200).
    */
   public function test_create_one_product(): void {
-    $faker = FakerFactory::create();
-
-    $token = auth()->attempt(['email' => 'admin@admin.com', 'password' => 'admin']);
-    $this->assertNotNull($token);
-
+    $faker        = FakerFactory::create();
     $product_data = [
       'image_url'         => $faker->url(),
       'category'          => $faker->name(),
@@ -53,14 +56,14 @@ class RequestsProductsTest extends TestCase {
         'landing_system'      => $faker->text(10),
       ],
     ];
-    $response = $this->withHeader('Authorization', 'Bearer ' . $token)->post('/api/admin/products', $product_data);
+    $response = $this->withHeader('Authorization', 'Bearer ' . $this->token)->post('/api/products', $product_data);
     $response->assertStatus(Response::HTTP_CREATED);
   }
 
   /**
-   * Sends a get request to /api/products/ and
-   * - Expects a OK (200) response.
-   * - Expects a total of 100 products.
+   * GET request to /api/products
+   * - Asserts that the response status code is OK (200).
+   * - Asserts that the total itens return is 100.
    */
   public function test_get_all_products(): void {
     $response = $this->get('/api/products');
@@ -71,15 +74,87 @@ class RequestsProductsTest extends TestCase {
   }
 
   /**
-   * Sends a get request to /api/products/{id}
-   * - Expects a OK (200) response.
+   * GET request to /api/products/{id}
+   * - Asserts that the response status code is OK (200).
+   * - Generates a random number between 1-100 and uses it as the id.
+   * - Asserts that the id from the product received is the same it sent.
    */
-  public function test_get_product_by_id(): void {
+  public function test_get_product_by_id_request(): void {
     $id       = mt_rand(1, 100);
     $response = $this->get('/api/products/' . $id);
     $response->assertStatus(Response::HTTP_OK);
     $response_json = $response->json();
     $product_id    = $response_json['id'];
     $this->assertEquals($id, $product_id);
+  }
+
+  /**
+   * GET request to /api/products/search?
+   * - Asserts that the response status code is OK (200).
+   * - Asserts that the total itens returned is higher than 0
+   */
+  public function test_search_query_request() {
+    $response = $this->get('/api/products/search?name[lk]=dr');
+    $response->assertStatus(200);
+    $response_json = $response->json();
+    $product_total = $response_json['total'];
+    $this->assertGreaterThan(0, $product_total);
+  }
+
+  /**
+   * UPDATE request to /api/products/{id}
+   * - Asserts that the response status code is OK (200).
+   * - Asserts that the returned product name was updated.
+   */
+  public function test_update_request() {
+    $update_data = ['name' => 'svhEftyçP'];
+    $request     = $this->withHeader('Authorization', 'Bearer ' . $this->token)->patch('/api/products/10', $update_data);
+    $request->assertStatus(200);
+    $request_json = $request->json();
+    $this->assertEquals($request_json['name'], 'svhEftyçP');
+  }
+
+  /**
+   * DELETE Request to /api/products/{id}
+   * - Asserts that the return data is 1.
+   * - Asserts that the response status code is OK (200).
+   * Tries to find the deleted record in the database.
+   * - Asserts that the response status code is NOT FOUND (404).
+   */
+  public function test_delete_request() {
+    $id       = mt_rand(1, 100);
+    $response = $this->withHeader('Authorization', 'Bearer ' . $this->token)->delete('/api/products/' . $id);
+    $response->assertStatus(Response::HTTP_OK);
+    $response_json = $response->json();
+    $this->assertEquals($response_json, 1);
+
+    $verify = $this->get('/api/products/' . $id);
+    $verify->assertStatus(404);
+  }
+
+  /**
+   * GET request to /api/products/miniatures
+   * - Asserts that the response status code is OK (200).
+   * - Asserts that the total itens return is 100.
+   */
+  public function test_get_all_miniatures() {
+    $response = $this->get('/api/products/miniatures');
+    $response->assertStatus(Response::HTTP_OK);
+    $response_json = $response->json();
+    $product_total = $response_json['total'];
+    $this->assertEquals(100, $product_total);
+  }
+
+  /**
+   * POST request to /api/products/miniatures
+   * - Asserts that the response status code is OK (200).
+   * - Asserts that the total itens return is 3.
+   */
+  public function test_get_some_miniatures() {
+    $ids      = [5, 75, 35];
+    $response = $this->post('/api/products/miniatures', ['products' => $ids]);
+    $response->assertStatus(Response::HTTP_OK);
+    $response_json = $response->json();
+    $this->assertEquals(count($response_json), 3);
   }
 }
